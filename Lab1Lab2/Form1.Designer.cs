7de14c0a3f0ea7a7313a0bd26048a5a673b8d493
Rechelle <116283200+Anover24@users.noreply.github.com>
2022-11-25 16:37:07 +0800

    Add files via upload

---
+++ Lab1Lab2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Rechelle
+{
+    partial class FrmLab1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.brnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(72, 23);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(235, 70);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // brnCreate
+            // 
+            this.brnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brnCreate.Location = new System.Drawing.Point(145, 120);
+            this.brnCreate.Name = "brnCreate";
+            this.brnCreate.Size = new System.Drawing.Size(75, 23);
+            this.brnCreate.TabIndex = 1;
+            this.brnCreate.Text = "Create";
+            this.brnCreate.UseVisualStyleBackColor = true;
+            this.brnCreate.Click += new System.EventHandler(this.brnCreate_Click);
+            // 
+            // FrmLab1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 170);
+            this.Controls.Add(this.brnCreate);
+            this.Controls.Add(this.txtInput);
+            this.Name = "FrmLab1";
+            this.Text = "FrmLab1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button brnCreate;
+    }
+}
+
---
+++ Lab1Lab2/FrmOpenFile.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Rechelle
+{
+    partial class FrmOpenFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvShowText = new System.Windows.Forms.ListView();
+            this.bntOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lvShowText
+            // 
+            this.lvShowText.HideSelection = false;
+            this.lvShowText.Location = new System.Drawing.Point(33, 12);
+            this.lvShowText.Name = "lvShowText";
+            this.lvShowText.Size = new System.Drawing.Size(313, 115);
+            this.lvShowText.TabIndex = 0;
+            this.lvShowText.UseCompatibleStateImageBehavior = false;
+            // 
+            // bntOpen
+            // 
+            this.bntOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntOpen.Location = new System.Drawing.Point(160, 170);
+            this.bntOpen.Name = "bntOpen";
+            this.bntOpen.Size = new System.Drawing.Size(67, 25);
+            this.bntOpen.TabIndex = 1;
+            this.bntOpen.Text = "Open";
+            this.bntOpen.UseVisualStyleBackColor = true;
+            this.bntOpen.Click += new System.EventHandler(this.bntOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FrmOpenFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 230);
+            this.Controls.Add(this.bntOpen);
+            this.Controls.Add(this.lvShowText);
+            this.Name = "FrmOpenFile";
+            this.Text = "FrmOpenFile";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvShowText;
+        private System.Windows.Forms.Button bntOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
